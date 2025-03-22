bc82a34240bbe7c9f9ee39ca0f329ff52f6054de
SwethaaB-CY <swethaachiselyourself@gmail.com>
2025-03-22 16:46:50 +0530

    FeatureSD update

--- frontend/src/components/student/FeatureSD.tsx
+++ frontend/src/components/student/FeatureSD.tsx
@@ -1,22 +1,24 @@
 import React from "react";
 import styles from "@/styles/student/FeatureSD.module.css";
-import { FaTasks, FaFlag, FaChartLine } from "react-icons/fa";
+import { FaTasks, FaFlag, FaChartLine, FaTools } from "react-icons/fa";
 import { useRouter } from "next/router";
 
 const FeatureSD: React.FC = () => {
   const router = useRouter();
 
   const handleNavigation = (title: string) => {
-    if (title === "Skill Assessment") {
-      router.push("/student/skillAssessment");
+    
+    if (title === "Skills") {
+      router.push("/student/skill");
     }
   };
 
   const features = [
-    { icon: <FaTasks size={40} color="#EE93BE" />, title: "Track Applications", desc: "Check status" },
-    { icon: <FaFlag size={40} color="#7FFFD4" />, title: "Skill Assessment", desc: "Take a test" },
-    { icon: <FaFlag size={40} color="#7FFFD4" />, title: "Mock Interview", desc: "Take a mock" },
-    { icon: <FaChartLine size={40} color="#F68C1F" />, title: "Career Progress", desc: "Check now" }
+    { icon: <FaTasks size={35} color="#EE93BE" />, title: "Track Applications", desc: "Check status" },
+    { icon: <FaFlag size={35} color="#7FFFD4" />, title: "Skill Assessment", desc: "Take a test" },
+    { icon: <FaFlag size={35} color="#7FFFD4" />, title: "Mock Interview", desc: "Take a mock" },
+    { icon: <FaChartLine size={35} color="#F68C1F" />, title: "Career Progress", desc: "Check now" },
+    { icon: <FaTools size={35} color="#FF6347" />, title: "Skills", desc: "Manage skills" }
   ];
 
   return (
@@ -26,7 +28,7 @@ const FeatureSD: React.FC = () => {
           key={index}
           className={styles.card}
           onClick={() => handleNavigation(feature.title)}
-          style={{ cursor: feature.title === "Skill Assessment" ? "pointer" : "default" }}
+          style={{ cursor: feature.title === "Skill Assessment" || feature.title === "Skills" ? "pointer" : "default" }}
         >
           <div className={styles.icon}>{feature.icon}</div>
           <h3>{feature.title}</h3>
--- frontend/src/pages/student/skill.tsx
+++ frontend/src/pages/student/skill.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Skills from "@/components/student/Skills";
-import styles from "@/styles/student/SkillAssessment.module.css";
+import styles from "@/styles/student/Skill.module.css";
 
 const SkillAssessment: React.FC = () => {
   return (
     <div className={styles.content}>
-      <h1 className={styles.heading}>Skill Assessment</h1>
+      <h1 className={styles.heading}>Update Your Skills </h1>
       <p className={styles.description}>Add your skills and select your expertise level.</p>
 
       {/* Skills Component */}
