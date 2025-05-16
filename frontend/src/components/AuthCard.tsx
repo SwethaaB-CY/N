7de4a0795ab9dc6ae585e18a48e8e2df6ab5a080
SwethaaB-CY <swethaa7103@gmail.com>
2025-05-16 12:06:46 +0530

    auth.css as global

--- frontend/src/components/AuthCard.tsx
+++ frontend/src/components/AuthCard.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
-import "../styles/auth.css";
 
 interface AuthCardProps {
   closeAuth: () => void;
