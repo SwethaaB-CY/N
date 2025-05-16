40c32a5c5fd406d95b000003f989153763c24f32
SwethaaB-CY <swethaa7103@gmail.com>
2025-05-16 10:19:43 +0530

    Update AuthCard.tsx

--- frontend/src/components/AuthCard.tsx
+++ frontend/src/components/AuthCard.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
-import "../styles/auth.css";
+import "../styles/auth.module.css";
 
 interface AuthCardProps {
   closeAuth: () => void;
