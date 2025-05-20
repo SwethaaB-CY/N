6c21edd8282dda6f45c84ead27a73e078ee12321
SwethaaB-CY <swethaa7103@gmail.com>
2025-05-20 20:58:07 +0530

    update

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,9 @@ import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module";
 import { StudentModule } from "./student/student.module";
 import { ConfigModule } from "@nestjs/config";
+import { InterviewModule } from './interview/interview.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, StudentModule],
+  imports: [ConfigModule.forRoot(), AuthModule, StudentModule, InterviewModule],
 })
 export class AppModule {}
